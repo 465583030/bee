e5ba046ae2534dbeb13cac7f8dc2ed9e8288c58b
465583030 <465583030@qq.com>
2017-03-16 19:54:18 +0800

    6th commit

--- example.go
+++ example.go
@@ -1,12 +1,24 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/465583030/bee/getproxy"
 	"github.com/465583030/bee/goreq"
 )
 
 func main() {
-	resp, body, err := goreq.New().Get("http://cn-proxy.com/").end
+	resp, body, err := goreq.New().Get("http://www.baidu.com/").End()
 	//defer resp
-	getproxy.Parse_cn_proxy(body)
+	//getproxy.Get(body)
+	fmt.Println(resp)
+	fmt.Println(body)
+	fmt.Println(err)
+
+	peers := getproxy.Get()
+	for _, v := range peers {
+		//fmt.Printf("index:%d  value:%d\n", i, v)
+		fmt.Println(v)
+	}
+	//fmt.Println()
 }
